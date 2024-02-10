11540ed288cc5e761fcd6301057a58b64ead3846
ayush931 <ayushkumar9315983@gmail.com>
2024-02-10 09:40:21 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
